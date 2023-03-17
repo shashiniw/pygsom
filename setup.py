bb41269b16f46f30cf212009b0184b3e5f119066
unknown <shashiniw@gmail.com>
2023-03-17 18:34:18 +0530

    node weights getter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pygsom",
+    name="pygsom_edit",
     version="0.1.0",
     author="Thimal Kempitiya",
     author_email="t.kempitiya@gmail.com",
